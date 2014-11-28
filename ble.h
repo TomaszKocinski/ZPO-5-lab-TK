673d23455fff8c5e62bd4319b6cb0e394b6d5029
kociak0 <kociak0@gmail.com>
2014-11-28 19:16:05 +0100

    Panownie siema
    JAK JA KURWA NIENAWIDZE TEGO

---
+++ ble.h
@@ -0,0 +1,21 @@
+#ifndef ble_cpp
+#define ble_cpp
+#include<iostream>
+#include<string>
+#include<vector>
+#include<list>
+#include<iterator>
+class OC:public std::vector<std::list<std::string>>{
+public:
+	void open(const char* filename);
+	OC(const char* filename){
+		open(filename);
+	};
+	OC() {};
+	void write(std::ostream& out = std::cout);
+	void execute();
+};
+struct GS {
+	bool operator() (const std::string&, const std::string&) const;
+};
+#endif
